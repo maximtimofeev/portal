e482ab120b6482c449e35d3b727ca509d0cebc66
maximtimofeev <makscarlos@hotmail.com>
2018-04-29 23:25:30 +0300

    css in js, reset-css

--- frontend/components/header/index.jsx
+++ frontend/components/header/index.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
-import css from './index.sass'
+import { css } from 'aphrodite/no-important'
+import styles from './style'
 
 class Header extends React.Component {
   render () {
     return (
-      <div>
+      <div className={css(styles.header)}>
+        <h1 className={css(styles.mainHeader)}>
+          Hello, im calculator
+        </h1>
       </div>
     );
   }
