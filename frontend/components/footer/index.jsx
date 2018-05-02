ec7cb3aefdd8662d9d3238283a97fa7b29aad3be
maximtimofeev <makscarlos@hotmail.com>
2018-05-03 01:03:23 +0300

    grid layout and components w/o wrapper div

--- frontend/components/footer/index.jsx
+++ frontend/components/footer/index.jsx
@@ -3,16 +3,14 @@ import PropTypes from "prop-types"
 import { css } from 'aphrodite/no-important'
 import styles from './style'
 
-class Header extends React.Component {
+class Footer extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <h1 className={css(styles.mainHeader)}>
-          Hello, im calculator
-        </h1>
+        <h2>Hello, i am the footer</h2>
       </React.Fragment>
     );
   }
 }
 
-export default Header
+export default Footer
--- frontend/components/header/style.js
+++ frontend/components/header/style.js
@@ -1,11 +1,6 @@
 import { StyleSheet } from 'aphrodite/no-important';
 
 export default StyleSheet.create({
-  header: {
-    width: '100%',
-    height: '100px',
-    background: 'green'
-  },
   mainHeader: {
     display: 'flex',
     color: '#ffffff'
