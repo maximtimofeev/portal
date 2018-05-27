d857d8e86fc9ddbe1481e110a27b0207505b0169
maximtimofeev <makscarlos@hotmail.com>
2018-05-27 18:04:05 +0300

    raw detal description

--- frontend/components/calculator/talent_tree/talent/description/Description.jsx
+++ frontend/components/calculator/talent_tree/talent/description/Description.jsx
@@ -6,23 +6,22 @@ import styles from './style'
 class Description extends React.Component {
   constructor (props) {
     super(props);
+    let text = this.props.config.description;
+    this.props.config.descValues.map((value, i) => {
+      let regex = new RegExp(`<${i}>`);
+      text = text.replace(regex, value[this.props.points == 0 ? 0 : this.props.points - 1]);
+    })
     this.state = {
-      name: this.props.config.name,
-      capacity: this.props.config.capacity,
-      points: this.props.points
+      text: text
     }
-  }
-  componentWillReceiveProps(props) {
-    this.setState({
-      points: props.points
-    });
   }
   render () {
     return (
       <React.Fragment>
         <div className={css(styles.description)}>
-          <div className={css(styles.descriptionItem, styles.talentName)}>{this.state.name}</div>
-          <div className={css(styles.descriptionItem)}>Rank {this.state.points}/{this.state.capacity}</div>
+          <div className={css(styles.descriptionItem, styles.talentName)}>{this.props.config.name}</div>
+          <div className={css(styles.descriptionItem)}>Rank {this.props.points}/{this.props.config.capacity}</div>
+          <div className={css(styles.descriptionItem)}>{this.state.text}</div>
           <div className={css(styles.descriptionItem, styles.learnTips)}>
             <span className={css(styles.learn)}>Left Click To Learn</span>
             <span className={css(styles.unlearn)}>Right Click To Unlearn</span>
